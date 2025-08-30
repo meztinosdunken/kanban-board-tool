e903af62589455b154b394667467c16b8f5d4432
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-30 00:38:13 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kanban Board</h1>
+      {/* Kanban Board components will go here */}
+    </div>
+  );
+}
+
+export default App;
